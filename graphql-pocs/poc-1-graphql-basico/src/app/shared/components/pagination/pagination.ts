727004bbe8ffa75660880415896c1d1940298a47
Oscar Geovany Rico <geo981202@gmail.com>
2026-02-28 10:01:44 -0500

    Delete app

--- graphql-pocs/poc-1-graphql-basico/src/app/shared/components/pagination/pagination.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pagination',
-  imports: [],
-  templateUrl: './pagination.html',
-  styleUrl: './pagination.scss',
-})
-export class Pagination {}

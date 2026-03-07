6f42a93deba325437911911b47fa5fe96940da21
Oscar Geovany Rico <geo981202@gmail.com>
2026-03-06 19:36:50 -0500

    delete: remove poc-1-graphql-basico Angular project files and configurations

--- graphql-pocs/poc-1-graphql-basico/src/app/shared/head/head.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-head',
-  imports: [],
-  templateUrl: './head.html',
-  styleUrl: './head.scss',
-})
-export class Head {}
